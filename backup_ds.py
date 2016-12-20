f1b30434113711354f83fe4d49f6e7836e5376ea
Pavel.Malko <pavel.malko@ksyste.ms>
2016-12-21 00:26:19 +0200

    Correct helper module. Make backup threaded.

--- backup_ds.py
+++ backup_ds.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8
 
 import sys
+import threading
+
+import math
+
+from datetime import date, datetime
+from paramiko import AuthenticationException
+
 sys.path.insert(1, '/home/erkki/.local/lib/python2.6/site-packages/ecdsa-0.13-py2.6.egg/')
 sys.path.insert(1, '/home/erkki/.local/lib/python2.6/site-packages/requests-2.9.1-py2.6.egg')
 sys.path.insert(1, '/home/erkki/.local/lib/python2.6/site-packages/paramiko-1.16.0-py2.6.egg')
@@ -15,37 +22,26 @@ import getpass
 import os
 from socket import gethostbyname, gaierror
 from scp import SCPClient
+from ds_helper import ds_print, RE, extract
 
-parser = optparse.OptionParser(description='Get config from DS\'s and move them to 1.140', usage="usage: %prog [file with ds list]")
-#parser.add_option( help='Path to file with list of ds', required=True)
+AUTHORISE_TRY_COUNT, \
+CONNECT_TRY_INTERVAL = 5, 7
+io_lock = threading.Lock
 
-(options, args) = parser.parse_args()
-if len(args) != 1:
-    parser.error("incorrect number of arguments")
-
-list_file = os.path.abspath(args[0])
-if not os.path.isfile(list_file):
-    print('!!! file ' + list_file + ' does not exist')
-    sys.exit()
-
-user = getpass.getuser()
-secret = getpass.getpass('Pssword for DS:')
-
-while True:
-    st = raw_input("Enter a for \"After\" or b for \"Before\"\n: ")
-    if st == 'a' or st == 'b':
-        break
-
-name = time.strftime("%y%m%d_") + st + '_upgrade_'
-#For debug uncomment
-#paramiko.common.logging.basicConfig(level=paramiko.common.DEBUG)
 
 def get_file_name(ds, user, secret):
-
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=ds, username=user, password=secret, port=22, look_for_keys=False, allow_agent=False)
-    print("*** SSH esteblish with " + ds)
+    for k in range(AUTHORISE_TRY_COUNT):
+        try:
+            client.connect(hostname=ds, username=user, password=secret, port=22, look_for_keys=False, allow_agent=False)
+        except AuthenticationException as e:
+            ds_print(ds, "Error while authorize.")
+            ds_print(ds, e.message)
+        except Exception as e:
+            pass
+        time.sleep(CONNECT_TRY_INTERVAL)
+    ds_print(ds, "*** SSH establish with ", io_lock)
     channel = client.invoke_shell()
 
     channel.send("\n")
@@ -62,20 +58,29 @@ def get_file_name(ds, user, secret):
 
     prim_conf = re.findall(r'primary-config.*', printout).pop()
     res = re.findall(r'cf1:.*cfg', prim_conf).pop()
-    print('*** Config file name ' + res)
+    ds_print(ds, '*** Config file name ' + res, io_lock)
 
     return res
 
 
-def get_file(ds, user, secret, file_name):
+def get_file(ds, user, secret, name, file_name):
     dest = name + file_name
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(ds, 22, user, secret)
-    print("*** SCP connect esteblish " + ds)
+    for k in range(AUTHORISE_TRY_COUNT):
+        try:
+            client.connect(ds, 22, user, secret)
+        except AuthenticationException as e:
+            ds_print(ds, "Error while authorize.")
+            ds_print(ds, e.message)
+        except Exception as e:
+            pass
+        time.sleep(CONNECT_TRY_INTERVAL)
+
+    ds_print(ds, "*** SCP connect establish ", io_lock)
     scp = SCPClient(client.get_transport())
-    print('*** Get file ' + file_name + ' from ' + ds)
+    ds_print(ds, '*** Get file ' + file_name + ' from ' + ds, io_lock)
     scp.get(file_name, dest)
     return dest
 
@@ -83,26 +88,74 @@ def mv_to_140(ds, config):
     remote_dir = '/mnt/om_kie/Backups/DS/' + ds + '/' + time.strftime("%Y") + '/'
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('10.44.4.28', username='smdmud\\stscheck_script',  key_filename='/home/butko/script/ds_bkp/.id_script_dsa')
+    for k in range(AUTHORISE_TRY_COUNT):
+        try:
+            ssh.connect('10.44.4.28', username='smdmud\\stscheck_script',  key_filename='/home/butko/script/ds_bkp/.id_script_dsa')
+            break
+        except AuthenticationException as e:
+            ds_print(ds, "Error while authorize.")
+            ds_print(ds, e.message)
+        except Exception as e:
+            pass
+        time.sleep(CONNECT_TRY_INTERVAL)
 
     scp = SCPClient(ssh.get_transport())
-    print('*** Move file ' + config + ' to ' + remote_dir)
+    ds_print(ds, '*** Move file ' + config + ' to ' + remote_dir, io_lock)
     scp.put(config , remote_dir )
     os.remove(config)
 
-
-with open(list_file) as f:
-    dss = f.readlines()
-
-
-print(dss)
-
-for DS in dss:
-    DS = DS.strip()
+def copy_ds_backup(DS, user, secret, name):
     try:
-        file_name = get_file_name(DS, user, secret).replace('cf1:\\', '')
-        conf_file = get_file(DS, user, secret, file_name)
-        mv_to_140(DS, conf_file)
+        mv_to_140(DS, get_file(DS, user, secret, name, get_file_name(DS, user, secret).replace('cf1:\\', '')))
     except gaierror:
-        print('!!! ' + DS + ' Does not exist')
+        ds_print(DS, '!!! Does not exist', io_lock)
 
+
+parser = optparse.OptionParser(description='Get config from DS\'s and move them to 1.140', usage="usage: %prog [file with ds list]")
+parser.add_option("-f", "--file", dest="ds_list_file_name",
+                     help="file with list DS", metavar="FILE")
+#parser.add_option( help='Path to file with list of ds', required=True)
+
+(options, args) = parser.parse_args()
+if not options.ds_list_file_name or not args:
+    parser.error("Use [-f <ds list file> | ds ds ds ...]")
+
+ds_list = args
+if options.ds_list_file_name:
+    try:
+        with open(options.ds_list_file_name) as ds_list_file:
+            for line in ds_list_file.readlines():
+                ds_list.append(extract(RE.DS_NAME, line))
+    except IOError as e:
+        print "Error while open file: {file}".format(file=options.ds_list_file_name)
+        print e.message
+
+if len(ds_list) < 1:
+    print "No ds found in arguments."
+    exit()
+
+user = getpass.getuser()
+secret = getpass.getpass('Password for DS:')
+
+while True:
+    st = raw_input("Enter a for \"After\" or b for \"Before\"\n: ")
+    if st == 'a' or st == 'b':
+        break
+
+name = time.strftime("%y%m%d_") + st + '_upgrade_'
+
+#paramiko.common.logging.basicConfig(level=paramiko.common.DEBUG)
+
+print "Start running: {time}".format(time.strftime("%H:%m:%s"))
+if len(ds_list) == 1:
+    copy_ds_backup(ds_list[0], user, secret, name)
+else:
+    threads = list()
+    for ds in ds_list:
+        thread = threading.Thread(target=copy_ds_backup, name=ds, args=(ds, user, secret, name))
+        thread.start()
+        threading.appent(thread)
+
+    for thread in threading:
+        thread.join()
+print "Finish running: {time}".format(time.strftime("%H:%m:%s"))
--- ds_helper.py
+++ ds_helper.py
@@ -6,6 +6,7 @@ class RE:
     FILE_DATE_STRING = r'\b\d\d\/\d\d\/\d\d\d\d\b'
     FILE_TIME_STRING = r'\b\d\d:\d\d[am]\b'
     PRIMARY_BOF_IMAGE = re.compile(r'primary-image\s+?(\S+)\b', FLAGS)
+    SECONDARY_BOF_IMAGE = re.compile(r'secondary-image\s+?(\S+)\b', FLAGS)
     FILE_DATE = re.compile(FILE_DATE_STRING)
     FILE_TIME = re.compile(FILE_TIME_STRING)
     DIR_FILE_PREAMBLE = re.compile(FILE_DATE_STRING+r'\s+?'+FILE_TIME_STRING+r'\s+?(?:<DIR>|\d+?)\s+?', FLAGS)
@@ -18,16 +19,67 @@ class RE:
     '''
     SW_VERSION = re.compile(r'TiMOS-\w-\d\.\d\.R\d+?\b', FLAGS)
     FREE_SPACE_SIZE = re.compile(r'\b(\d+?)\s+?bytes free\.', FLAGS)
-    DS_NAME = re.compile(r'ds\d-[0-9a-z]+\b', FLAGS)
+    DS_NAME = re.compile(r'\bds\d-[0-9a-z]+\b', FLAGS)
 
-    def extract(regExp, text):
-        try:
-            return re.findall(regExp, text, re.IGNORECASE)[0]
-        except IndexError as error:
+
+def extract(regexp, text, flags=re.IGNORECASE):
+    """
+
+    :param regexp: regular expression
+    :param text: source for extracting
+    :param flags: default re.IGNORECASE Only for string regexp arguments
+    :return: first occur regular expression
+    """
+    try:
+        if regexp.__class__.__name__ == 'SRE_Pattern':
+            return regexp.findall(text)[0]
+        elif regexp.__class__.__name__ == str.__class__.__name__:
+            return re.findall(regexp, text, flags)[0]
+        else:
             return None
+    except IndexError as error:
+        return None
 
-    def is_contains(regExp, text, flags = re.IGNORECASE):
-        if re.search(regExp, text, flags):
+
+def is_contains(regexp, text, flags=re.IGNORECASE):
+    """
+
+    :param regexp:
+    :param text:
+    :param flags: default re.IGNORECASE Only for string regexp arguments
+    :return: True if string contains regular expression
+    """
+    if regexp.__class__.__name__ == 'SRE_Pattern':
+        if regexp.search(text):
             return True
         else:
             return False
+    elif regexp.__class__.__name__ == str.__class__.__name__:
+        if re.search(regexp, text, flags):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def ds_print(ds, message, io_lock=None):
+    """
+    Thread safe printing with DS in start line.
+
+    :param ds:
+    :param message:
+    :param io_lock: object threading.Lock or threading.RLock
+    """
+    if io_lock: io_lock.acquire()
+    print "{ds} : {message}".format(ds=ds, message=message)
+    if io_lock: io_lock.release()
+
+if __name__ == "__main__":
+    print RE.DS_NAME.findall(open('20161205_121503_Figaro.log').read())
+    print RE.DS_TYPE.findall(open('20161205_121503_Figaro.log').read())
+    print RE.PRIMARY_BOF_IMAGE.findall(open('20161205_121503_Figaro.log').read())
+    print RE.SECONDARY_BOF_IMAGE.findall(open('20161205_121503_Figaro.log').read())
+    print RE.DIR_FILE_PREAMBLE.findall(open('20161205_121503_Figaro.log').read())
+    print RE.FREE_SPACE_SIZE.findall(open('20161205_121503_Figaro.log').read())
+    print RE.SW_VERSION.findall(open('20161205_121503_Figaro.log').read())
