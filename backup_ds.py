dd72562952c675b217d4e1e6d9915ce9dbec3d43
Pavel.Malko <pavel.malko@ksyste.ms>
2016-12-26 17:38:22 +0200

    Finale version

--- backup_ds.py
+++ backup_ds.py
@@ -21,7 +21,6 @@ from ds_helper import ds_print, RE, extract
 
 AUTHORISE_TRY_COUNT, \
 CONNECT_TRY_INTERVAL = 5, 7
-io_lock = threading.Lock
 
 
 def get_file_name(ds, user, secret):
@@ -64,43 +63,37 @@ def get_file_name(ds, user, secret):
 
 
 def get_file(ds, user, secret, name, file_name):
-    ds_print(ds, "backup_ds.get_file - start")
     dest = name + file_name
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ds_print(ds, "backup_ds.get_file - authorize")
+
     for k in range(AUTHORISE_TRY_COUNT):
         try:
             client.connect(ds, 22, user, secret)
             break
         except AuthenticationException as e:
-            ds_print(ds, "Error while authorize: ")
-            ds_print(ds, e)
-            # ds_print(ds, e.message)
+            ds_print(ds, "Error while authorize: " + str(e))
         except Exception as e:
-            ds_print(ds, "Error:: ")
-            ds_print(ds, e)
+            ds_print(ds, "Error:: " + str(e))
         time.sleep(CONNECT_TRY_INTERVAL)
     else:
         ds_print(ds, "Can`t authorize")
         raise Exception("Can`t authorize on " + ds)
 
-    ds_print(ds, "backup_ds.get_file - authorized")
     ds_print(ds, "*** SCP connect establish ")
     scp = SCPClient(client.get_transport())
     ds_print(ds, '*** Get file ' + file_name + ' from ' + ds)
     scp.get(file_name, dest)
-    ds_print(ds, "backup_ds.get_file return: " + dest)
+
     return dest
 
 
 def mv_to_140(ds, config):
-    ds_print(ds, "backup_ds.mv_to_140 - start")
     remote_dir = '/mnt/om_kie/Backups/DS/' + ds + '/' + time.strftime("%Y") + '/'
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ds_print(ds, 'backup_ds.mv_to_140 - authorise')
+
     for k in range(AUTHORISE_TRY_COUNT):
         try:
             ssh.connect('10.44.4.28', username='smdmud\\stscheck_script',  key_filename='/home/butko/script/ds_bkp/.id_script_dsa')
@@ -128,9 +121,7 @@ def copy_ds_backup(DS, user, secret, name):
     except gaierror:
         ds_print(DS, '!!! Does not exist')
     except BaseException as e:
-        ds_print(DS, 'Error wile backup: ')
-        ds_print(e)
-
+        ds_print(DS, 'Error wile backup: ' + str(e))
 
 
 parser = optparse.OptionParser(description='Get config from DS\'s and move them to 1.140',
@@ -143,7 +134,6 @@ parser.add_option("-f", "--file", dest="ds_list_file_name",
 if not options.ds_list_file_name and not args:
     parser.error("Use [-f <ds list file> | ds ds ds ...]")
 
-paramiko.common.logging.basicConfig(level=paramiko.common.INFO)
 ds_list = args
 if options.ds_list_file_name:
     try:
